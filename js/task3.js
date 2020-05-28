18fa08d251b528e489aa6252c0870002476cc9d6
unknown <yaroslavdutka@gmail.com>
2020-05-28 15:15:46 +0300

    second try

--- js/task3.js
+++ js/task3.js
@@ -10,7 +10,10 @@ const makeTransaction = (transaction) => {
       const canProcess = Math.random() > 0.3;
 
       if (canProcess) {
-        resolve(transaction.id);
+        resolve({
+          id: transaction.id,
+          time: delay
+        });
       } else {
         reject(transaction.id);
       }
@@ -18,7 +21,10 @@ const makeTransaction = (transaction) => {
   });
 };
 
-const logSuccess = (id, ...time) => {
+const logSuccess = ({
+  id,
+  time
+}) => {
   console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
